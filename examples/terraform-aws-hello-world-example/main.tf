94cd3618c4b51895d49580292d30809f4fff2aee
Mat Moore <MatMoore@users.noreply.github.com>
2023-08-30 17:17:44 +0100

    Revert changes to use localstack
    It seems like there is still active development to make terratest work well with localstack.
    See https://github.com/gruntwork-io/terratest/pull/495

--- examples/terraform-aws-hello-world-example/main.tf
+++ examples/terraform-aws-hello-world-example/main.tf
@@ -7,45 +7,14 @@ terraform {
   required_version = ">= 0.12.26"
 }
 
-# see https://registry.terraform.io/providers/hashicorp/aws/latest/docs/guides/custom-service-endpoints#localstack
 provider "aws" {
-  access_key                  = "mock_access_key"
-  region                      = "us-east-1"
-  s3_use_path_style           = true
-  secret_key                  = "mock_secret_key"
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-
-  endpoints {
-    apigateway     = "http://localhost:4566"
-    cloudformation = "http://localhost:4566"
-    cloudwatch     = "http://localhost:4566"
-    dynamodb       = "http://localhost:4566"
-    es             = "http://localhost:4566"
-    firehose       = "http://localhost:4566"
-    iam            = "http://localhost:4566"
-    kinesis        = "http://localhost:4566"
-    lambda         = "http://localhost:4566"
-    route53        = "http://localhost:4566"
-    redshift       = "http://localhost:4566"
-    s3             = "http://localhost:4566"
-    secretsmanager = "http://localhost:4566"
-    ses            = "http://localhost:4566"
-    sns            = "http://localhost:4566"
-    sqs            = "http://localhost:4566"
-    ssm            = "http://localhost:4566"
-    stepfunctions  = "http://localhost:4566"
-    sts            = "http://localhost:4566"
-    ec2            = "http://localhost:4566"
-  }
+  region = "us-west-1"
 }
 
 # Deploy an EC2 Instance.
 resource "aws_instance" "example" {
-  # ubuntu ami
-  # list localstack amis with `awslocal ec2 describe-images --filters Name=tag:ec2_vm_manager,Values=docker`
-  ami                    = "ami-df5de72bdb3b"
+  # Run an Ubuntu 18.04 AMI on the EC2 instance.
+  ami                    = "ami-0d5d9d301c853a04a"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
 
@@ -69,6 +38,6 @@ resource "aws_security_group" "instance" {
 
 # Output the instance's public IP address.
 output "public_ip" {
-  value = aws_instance.example.public_ip # ! This does not work on localstack !
+  value = aws_instance.example.public_ip
 }
 
